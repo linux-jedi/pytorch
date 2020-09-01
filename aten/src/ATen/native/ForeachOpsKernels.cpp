e0424551f9737ad253f72326f0d5480b060213d4
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-09-01 12:27:25 -0700

    Added binary ops
    ghstack-source-id: 36c0128608741bddbf90f109ecb933adc79c640f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42536

--- aten/src/ATen/native/ForeachOpsKernels.cpp
+++ aten/src/ATen/native/ForeachOpsKernels.cpp
@@ -23,6 +23,63 @@ void foreach_tensor_add_scalar_kernel_slow_(TensorList tensors, Scalar scalar) {
   }
 }
 
+std::vector<Tensor> foreach_tensor_sub_scalar_kernel_slow(TensorList tensors, Scalar scalar) {
+  check_foreach_api_restrictions(tensors);
+
+  std::vector<Tensor> result;
+  for (const auto& t: tensors) {
+    result.emplace_back(t.sub(scalar));
+  }
+
+  return result;
+}
+
+void foreach_tensor_sub_scalar_kernel_slow_(TensorList tensors, Scalar scalar) {
+  check_foreach_api_restrictions(tensors);
+
+  for (auto& t: tensors) {
+    t.sub_(scalar);
+  }
+}
+
+std::vector<Tensor> foreach_tensor_div_scalar_kernel_slow(TensorList tensors, Scalar scalar) {
+  check_foreach_api_restrictions(tensors);
+
+  std::vector<Tensor> result;
+  for (const auto& t: tensors) {
+    result.emplace_back(t.div(scalar));
+  }
+
+  return result;
+}
+
+void foreach_tensor_div_scalar_kernel_slow_(TensorList tensors, Scalar scalar) {
+  check_foreach_api_restrictions(tensors);
+
+  for (auto& t: tensors) {
+    t.div_(scalar);
+  }
+}
+
+std::vector<Tensor> foreach_tensor_mul_scalar_kernel_slow(TensorList tensors, Scalar scalar) {
+  check_foreach_api_restrictions(tensors);
+
+  std::vector<Tensor> result;
+  for (const auto& t: tensors) {
+    result.emplace_back(t.mul(scalar));
+  }
+
+  return result;
+}
+
+void foreach_tensor_mul_scalar_kernel_slow_(TensorList tensors, Scalar scalar) {
+  check_foreach_api_restrictions(tensors);
+
+  for (auto& t: tensors) {
+    t.mul_(scalar);
+  }
+}
+
 std::vector<Tensor> foreach_tensor_add_list_kernel_slow(TensorList tensors1, TensorList tensors2) {
   check_foreach_api_restrictions(tensors1, tensors2);
 
@@ -40,6 +97,63 @@ void foreach_tensor_add_list_kernel_slow_(TensorList tensors1, TensorList tensor
 
   for (int i = 0; i < tensors1.size(); i++) {
     tensors1[i].add_(tensors2[i]);
+  }
+}
+
+std::vector<Tensor> foreach_tensor_sub_list_kernel_slow(TensorList tensors1, TensorList tensors2) {
+  check_foreach_api_restrictions(tensors1, tensors2);
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors1.size(); i++) {
+    result.emplace_back(tensors1[i].sub(tensors2[i]));
+  }
+
+  return result;
+}
+
+void foreach_tensor_sub_list_kernel_slow_(TensorList tensors1, TensorList tensors2) {
+  check_foreach_api_restrictions(tensors1, tensors2);
+
+  for (int i = 0; i < tensors1.size(); i++) {
+    tensors1[i].sub_(tensors2[i]);
+  }
+}
+
+std::vector<Tensor> foreach_tensor_mul_list_kernel_slow(TensorList tensors1, TensorList tensors2) {
+  check_foreach_api_restrictions(tensors1, tensors2);
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors1.size(); i++) {
+    result.emplace_back(tensors1[i].mul(tensors2[i]));
+  }
+
+  return result;
+}
+
+void foreach_tensor_mul_list_kernel_slow_(TensorList tensors1, TensorList tensors2) {
+  check_foreach_api_restrictions(tensors1, tensors2);
+
+  for (int i = 0; i < tensors1.size(); i++) {
+    tensors1[i].mul_(tensors2[i]);
+  }
+}
+
+std::vector<Tensor> foreach_tensor_div_list_kernel_slow(TensorList tensors1, TensorList tensors2) {
+  check_foreach_api_restrictions(tensors1, tensors2);
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors1.size(); i++) {
+    result.emplace_back(tensors1[i].div(tensors2[i]));
+  }
+
+  return result;
+}
+
+void foreach_tensor_div_list_kernel_slow_(TensorList tensors1, TensorList tensors2) {
+  check_foreach_api_restrictions(tensors1, tensors2);
+
+  for (int i = 0; i < tensors1.size(); i++) {
+    tensors1[i].div_(tensors2[i]);
   }
 }
 
